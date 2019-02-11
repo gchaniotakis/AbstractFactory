cfdfa475968df727c7c5afabc2b7933d7cba644d
George Chaniotakis <haniotinho@gmail.com>
2019-02-11 21:10:52 +0200

    Avensis Factory and Yaris Factory classes

---
+++ AbstractFactory/AvensisFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    class AvensisFactory : ToyotaFactory
+    {
+        public override Body MakeBody()
+        {
+            return new AvensisBody();
+        }
+
+        public override Engine MakeEngine()
+        {
+            return new AvensisEngine();
+        }
+    }
+}
---
+++ AbstractFactory/YarisFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    class YarisFactory : ToyotaFactory
+    {
+        public override Body MakeBody()
+        {
+            return new YarisBody();            
+        }
+
+        public override Engine MakeEngine()
+        {
+            return new YarisEngine();            
+        }
+    }
+}
