147ea4dec0c9f7a714eaed37db6a06eb8d777b9e
George Chaniotakis <haniotinho@gmail.com>
2019-02-11 21:33:11 +0200

    Car class

---
+++ AbstractFactory/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    class Car
+    {
+        public Body Body { get; }
+
+        public Engine Engine { get; }
+
+        public Car (Body body, Engine engine)
+        {
+            Body = body;
+
+            Engine = engine;
+        }
+
+
+        public override string ToString()
+        {
+            return $"{Body} with a weight of {Body.Weight}kg and a {Engine}";
+
+        }
+    }
+
+}
