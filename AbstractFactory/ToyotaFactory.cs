f3df7e9dfa2d0eefd4cb97c914704402b142815e
George Chaniotakis <haniotinho@gmail.com>
2019-02-11 20:39:13 +0200

    toyota factory class

---
+++ AbstractFactory/ToyotaFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    abstract class ToyotaFactory
+    {
+        abstract public Body MakeBody();
+
+        abstract public Engine MakeEngine();
+    }
+
+
+    abstract class Body
+    {
+        public int Weight { get; set; }
+    }
+
+
+    abstract class Engine
+    {
+        public abstract void Start();
+    }
+}
